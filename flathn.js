2ddc5713f8cd6099dcc34d3cbb96e9eb352ba411
Sagar G V <sagar.writeme@gmail.com>
2014-06-20 16:38:41 +0530

    Reducing font size for depth=1 comments works

--- flathn.js
+++ flathn.js
@@ -4,19 +4,25 @@ if(location.hostname == "news.ycombinator.com") {
         return ((e.src == "https://news.ycombinator.com/s.gif") && (e.width % 40 == 0) && (e.height == 1)); 
     });
     comments = comments.map(function(e) {
-        return { 'depth': e.width/40, 'ele': e.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement };
+        return { 'depth': e.width/40, // Integer
+                 'rowele': e.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement, // HTML <tr> for the comment
+                 'txt': e.parentElement.parentElement.lastChild.children[2] }; // HTML <span> for the comment. Comment text enclosed
     });
     if(typeof(__flathn__) == "undefined") {
         __flathn__ = true;
         for(var i=0;i < comments.length; i++) {
-            if(comments[i].depth != 0) {
-                comments[i].ele.style.display = "none";
+            if(comments[i].depth > 1) {
+                comments[i].rowele.style.display = "none";
+            }
+            if(comments[i].depth == 1) {
+                comments[i].txt.style.fontSize = "0.7em";
             }
         } 
     } else {
         delete __flathn__;
         for(var i=0;i < comments.length; i++) {
-            comments[i].ele.style.display = "";
+            comments[i].rowele.style.display = "";
+            comments[i].txt.style.fontSize = "";
         }
     }
 } else {
