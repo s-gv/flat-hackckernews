30f904ca92d94cd6ba2f21cbb7a0f4a886ef6b7e
Sagar G V <sagar.writeme@gmail.com>
2014-06-21 12:38:52 +0530

    Added explanations

--- flathn.js
+++ flathn.js
@@ -1,12 +1,15 @@
 if(location.hostname == "news.ycombinator.com") {
-    var spacerImages = document.getElementsByTagName("img");
+    var spacerImages = document.getElementsByTagName("img"); // get all images because HN uses a small image for indenting
     var comments = Array.prototype.slice.call(spacerImages).filter(function (e) {
+        // filter only spacer images to get near comments.
+        // spacer images have widths as multiples of 40
+        // Chrome returns height as 2 and Firefox as 1. So check both
         return ((e.src == "https://news.ycombinator.com/s.gif") && (e.width % 40 == 0) && (e.height == 1||e.height == 2)); 
     });
     comments = comments.map(function(e) {
-        return { 'depth': e.width/40, // Integer
+        return { 'depth': e.width/40, // Integer indicating depth of comment. depth = 0 is the main commet, 1 are replies to it etc.
                  'rowele': e.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement, // HTML <tr> for the comment
-                 'txt': e.parentElement.parentElement.lastChild.children[2] }; // HTML <span> for the comment. Comment text enclosed
+                 'txt': e.parentElement.parentElement.lastChild.children[2] }; // HTML <span> for the comment. Comment text enclosed in this span
     });
     if(typeof(__flathn__) == "undefined") {
         __flathn__ = true;
@@ -15,7 +18,7 @@ if(location.hostname == "news.ycombinator.com") {
                 comments[i].rowele.style.display = "none";
             }
             if(comments[i].depth == 1) {
-                comments[i].txt.style.fontSize = "0.7em";
+                comments[i].txt.style.fontSize = "0.7em"; // reduce font size of depth=1 comment
                 comments[i].txt.parentElement.children[0].children[0].style.fontSize = "0.75em";
                 if(comments[i].txt.lastChild.textContent == "reply")
                     comments[i].txt.lastChild.style.display = "none";
