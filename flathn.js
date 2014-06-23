a0e6b177ede86a67369ee2f2946cd32992efe8b5
Sagar G V <sagar.writeme@gmail.com>
2014-06-23 17:52:51 +0530

    Show More seems to work

--- flathn.js
+++ flathn.js
@@ -16,26 +16,45 @@ if(location.hostname == "news.ycombinator.com") {
         __flathn__ = true;
         for(var i=0;i < comments.length; i++) {
             if(comments[i].depth > 1) {
+                // Don't display comments with depth > 1
                 comments[i].rowele.style.display = "none";
             }
-            if(comments[i].depth == 1) {
+            if(comments[i].depth >= 1) {
+                // Create a "Show More" HTML element
                 var pTag = document.createElement('p');
                 pTag.setAttribute('class', "showmore");
                 var aTag = document.createElement('a');
                 aTag.setAttribute('href',"#");
                 aTag.innerHTML = "Show More";
-                aTag.addEventListener("click", function(event) { 
-                    alert("bar");
-                    event.preventDefault();
-                });
+                aTag.addEventListener("click", (function(n) {
+                    return function(event) {
+                        // User really wants to see this. Show the deeper comments at smaller font.
+                        for(var j=n+1; j < comments.length; j++) {
+                            if(comments[j].depth < (comments[n].depth + 1)) {
+                                break;
+                            }
+                            if(comments[j].depth == (comments[n].depth + 1)) {
+                                comments[j].rowele.style.display = ""; // show the row
+                                comments[j].txt.lastChild.style.display = ""; // Show "reply"
+                                comments[j].txt.parentElement.lastChild.style.display = "";
+                            }
+                        }
+                        event.preventDefault();
+                    }
+                })(i), true);
                 pTag.appendChild(aTag);
+                // Don't display "reply" to deeper comments
                 if(comments[i].txt.lastChild.textContent == "reply")
                     comments[i].txt.lastChild.style.display = "none";
                 if(comments[i].txt.parentElement.lastChild.textContent == "reply")
                     comments[i].txt.parentElement.lastChild.style.display = "none";
-                comments[i].txt.appendChild(pTag);
-            }
-            if(comments[i].depth >= 1) {
+                if(i < (comments.length - 1)) {
+                    if(comments[i+1].depth > comments[i].depth) {
+                        // Display "Show More" only if there is something more to see.
+                        comments[i].txt.appendChild(pTag);
+                    }
+                }
+                // Reduce font size for deeper comments
                 comments[i].txt.style.fontSize = reducedFontSize; // reduce font size of depth=1 comment
                 comments[i].txt.parentElement.children[0].children[0].style.fontSize = reducedFontSize;
             }
@@ -49,6 +68,7 @@ if(location.hostname == "news.ycombinator.com") {
         for(var i=0;i < comments.length; i++) {
             comments[i].rowele.style.display = "";
             if(comments[i].depth >= 1) {
+                // Reset font-size and display props of all comments
                 comments[i].txt.style.fontSize = "";
                 comments[i].txt.parentElement.children[0].children[0].style.fontSize = "";
                 comments[i].txt.lastChild.style.display = "";
