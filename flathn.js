12b3718c4eda686bd25433eb35314ba91bdd702a
Sagar G V <sagar.writeme@gmail.com>
2014-06-23 13:49:58 +0530

    Show More dummy links add/remove working

--- flathn.js
+++ flathn.js
@@ -1,15 +1,16 @@
 if(location.hostname == "news.ycombinator.com") {
+    var reducedFontSize = "0.8em";
     var spacerImages = document.getElementsByTagName("img"); // get all images because HN uses a small image for indenting
     var comments = Array.prototype.slice.call(spacerImages).filter(function (e) {
         // filter only spacer images to get near comments.
         // spacer images have widths as multiples of 40
-        // Chrome returns height as 2 and Firefox as 1. So check both
+        // Chrome returns height as 2 and Firefox as 1. So accept both.
         return ((e.src == "https://news.ycombinator.com/s.gif") && (e.width % 40 == 0) && (e.height == 1||e.height == 2)); 
     });
     comments = comments.map(function(e) {
         return { 'depth': e.width/40, // Integer indicating depth of comment. depth = 0 is the main commet, 1 are replies to it etc.
-                 'rowele': e.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement, // HTML <tr> for the comment
-                 'txt': e.parentElement.parentElement.lastChild.children[2] }; // HTML <span> for the comment. Comment text enclosed in this span
+            'rowele': e.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement, // HTML <tr> for the comment
+             'txt': e.parentElement.parentElement.lastChild.children[2] }; // HTML <span> for the comment. Comment text enclosed in this span
     });
     if(typeof(__flathn__) == "undefined") {
         __flathn__ = true;
@@ -18,22 +19,35 @@ if(location.hostname == "news.ycombinator.com") {
                 comments[i].rowele.style.display = "none";
             }
             if(comments[i].depth == 1) {
-                comments[i].txt.style.fontSize = "0.7em"; // reduce font size of depth=1 comment
-                comments[i].txt.parentElement.children[0].children[0].style.fontSize = "0.75em";
+                var aTag = document.createElement('a');
+                aTag.setAttribute('href',"yourlink.htm");
+                aTag.setAttribute('class', "showmore");
+                aTag.innerHTML = "<p>Show More</p>";
                 if(comments[i].txt.lastChild.textContent == "reply")
                     comments[i].txt.lastChild.style.display = "none";
                 if(comments[i].txt.parentElement.lastChild.textContent == "reply")
                     comments[i].txt.parentElement.lastChild.style.display = "none";
+                comments[i].txt.appendChild(aTag);
+            }
+            if(comments[i].depth >= 1) {
+                comments[i].txt.style.fontSize = reducedFontSize; // reduce font size of depth=1 comment
+                comments[i].txt.parentElement.children[0].children[0].style.fontSize = reducedFontSize;
             }
         } 
     } else {
         delete __flathn__;
+        var showmores = document.getElementsByClassName("showmore");
+        while(showmores.length > 0) {
+            showmores[0].parentNode.removeChild(showmores[0]);
+        }
         for(var i=0;i < comments.length; i++) {
             comments[i].rowele.style.display = "";
-            comments[i].txt.style.fontSize = "";
-            comments[i].txt.parentElement.children[0].children[0].style.fontSize = "";
-            comments[i].txt.lastChild.style.display = "";
-            comments[i].txt.parentElement.lastChild.style.display = "";
+            if(comments[i].depth >= 1) {
+                comments[i].txt.style.fontSize = "";
+                comments[i].txt.parentElement.children[0].children[0].style.fontSize = "";
+                comments[i].txt.lastChild.style.display = "";
+                comments[i].txt.parentElement.lastChild.style.display = "";
+            }
         }
     }
 } else {
